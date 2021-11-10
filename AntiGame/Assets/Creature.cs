a13d6f254b5d30be9c4d64b2f51b8a0a2a42628f
denshust <denshust@ukr.net>
2021-11-10 19:55:53 +0200

    COLOURS
    hehe, colors

--- AntiGame/Assets/Creature.cs
+++ AntiGame/Assets/Creature.cs
@@ -7,15 +7,18 @@ public class Creature : MonoBehaviour
     public GameObject[] pieces;
     void Start()
     {
-        StartCoroutine(Set());
+        // StartCoroutine(Set());
+        SetAppearence();
     }
     void SetAppearence()
     {
+        Color color = Random.ColorHSV(0f,1f,1f,1f,1f,1f,1f,1f);
+        GetComponent<SpriteRenderer>().color = color;
         foreach (GameObject piece in pieces)
         {
+            if(piece.name!="face")
+            piece.GetComponent<SpriteRenderer>().color = color;
             int r = Random.Range(1, 5);
-            piece.GetComponent<Animator>().enabled = false;
-            piece.GetComponent<Animator>().enabled = true;
             piece.GetComponent<Animator>().Play(piece.name + r.ToString());
         }
     }
